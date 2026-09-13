0308e61629ee3f2356d31250f636ea3db5ddaaf2
ilia <mescheryakoviliaa@gmail.com>
2026-09-13 23:06:25 +0300

    change solution 2

--- basic_solutions.c
+++ basic_solutions.c
@@ -3,32 +3,6 @@
 #include <stdio.h>
 
 // Задача 2 - Максимум из трёх чисел
-// int max_of_three(int a, int b, int c) {
-//     if (a >= b && a >= c) {
-//         return a;
-//     } else if (b >= a && b >= c) {
-//         return b;
-//     } else {
-//         return c;
-//     }
-// }
-
-// int max_of_three(int a, int b, int c) {
-//     if (a > b) {
-//         if (a > c) {
-//             return a;
-//         } else {
-//             return c;
-//         }
-//     } else {
-//         if (b > c) {
-//             return b;
-//         } else {
-//             return c;
-//         }
-//     }
-// }
-
 int max_of_three(int a, int b, int c) {
     int max = a;
     if (b > max) {
